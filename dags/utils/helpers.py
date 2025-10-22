1cc68feb67328783c6fd157ca6c99f45a982e8b4
Lautaro Poletto <lautaropoletto@gmail.com>
2025-10-22 16:08:05 +0000

    Feature: Nuevo metodo get_spark_session

--- dags/utils/helpers.py
+++ dags/utils/helpers.py
@@ -1,8 +1,11 @@
+from datetime import datetime, timedelta
 from os import environ as env
 import logging
 from minio import Minio
 import psycopg2
-from datetime import datetime, timedelta
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, lit, regexp_extract, concat
 
 
 
@@ -115,3 +118,32 @@ def fetch_sql_query_result(sql_query: str, db_name: str):
     finally:
         conn.close()
     return results
+
+
+def get_spark_session() -> SparkSession:
+    """
+    Crea y retorna una session de Spark.
+    """
+    # Configuración de SparkSession con soporte s3 y MinIO
+    DRIVER_PATH = env["DRIVER_PATH"]
+
+    spark = SparkSession.builder.master("local[1]") \
+        .appName("ETL Spark") \
+        .config("spark.jars", DRIVER_PATH) \
+        .config("spark.executor.extraClassPath", DRIVER_PATH) \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+        .config("spark.hadoop.fs.s3a.access.key", env["MINIO_ROOT_USER"]) \
+        .config("spark.hadoop.fs.s3a.secret.key", env["MINIO_ROOT_PASSWORD"]) \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .getOrCreate()
+    
+    return spark
+
+
+def add_ingestion_date(input_df, custom_date_column="ingestion_date"):
+    # yesterday = datetime.now() - timedelta(days=1)
+    # v_date = yesterday.strftime("%Y-%m-%d") # Formato: 2025-06-10
+    v_date = current_timestamp()
+    output_df = input_df.withColumn(custom_date_column, v_date)
+    return output_df
