6fca902b4d119524ab5272e0554389d917bc900b
Lautaro Poletto <lautaropoletto@gmail.com>
2025-10-28 18:13:04 +0000

    Add pipelines para cargas historicas

---
+++ scripts/ingest_history_pit_stops_to_bronze.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import sys
+from os import environ as env
+from helpers import *
+
+
+if __name__ == "__main__":
+    spark = get_spark_session()
+    execution_date = sys.argv[1]
+    table_name = "pitStops"
+    
+    print(f"\nFecha de ejecución: {execution_date}")
+    print(f"Tabla/Query a procesar: {table_name}")
+
+    sql_query = f"""
+    SELECT ps.raceId, 
+        driverId, 
+        stop, 
+        lap, 
+        ps.`time`, 
+        duration, 
+        milliseconds
+    FROM f1db.{table_name} ps
+    INNER JOIN f1db.races r on ps.raceId = r.raceId
+    WHERE r.`date` <= '{execution_date}'
+    """
+   
+    ingest_to_bronze(spark, table_name, sql_query, execution_date)   
+    # Detener la sesión de Spark
+    spark.sparkContext.stop()
---
+++ scripts/ingest_history_results_to_bronze.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import sys
+from os import environ as env
+from helpers import *
+
+
+if __name__ == "__main__":
+    spark = get_spark_session() # Configuración de SparkSession con soporte S3
+    execution_date = sys.argv[1] 
+    table_name = "results"
+    
+    print(f"\nFecha de ejecución: {execution_date}")
+    print(f"Tabla/Query a procesar: {table_name}")
+
+    sql_query = f"""
+    SELECT resultId, 
+        res.raceId, 
+        driverId, 
+        constructorId, 
+        `number`, 
+        grid, 
+        `position`, 
+        positionText, 
+        positionOrder, 
+        points, 
+        laps, 
+        res.`time`, 
+        milliseconds, 
+        fastestLap, 
+        `rank`, 
+        fastestLapTime, 
+        fastestLapSpeed, 
+        statusId
+    FROM f1db.{table_name} res
+    INNER join f1db.races r on res.raceId = r.raceId
+    WHERE r.`date` <= '{execution_date}'
+    """
+   
+    ingest_to_bronze(spark, table_name, sql_query, execution_date)   
+    # Detener la sesión de Spark
+    spark.sparkContext.stop()
