204ca49e4e5a0255f6364a44ab6ee95977c1653f
Lautaro Poletto <lautaropoletto@gmail.com>
2025-10-29 19:07:11 +0000

    Add: se agregan pl para cargas historicas

---
+++ dags/incremental_load/pl_historical_qualifying.py
@@ -0,0 +1,48 @@
+from os import environ as env
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.models import Variable
+
+default_args = {
+    "owner": "Lautaro",
+    "start_date": datetime(2025, 9, 29),
+    "retries": 1,
+    "retry_delay": timedelta(seconds=5),
+    "catchup": False
+}
+
+with DAG(
+    dag_id="qualifying_historical_load_etl",
+    default_args=default_args,
+    description="Carga de datos de la tabla qualifying",
+    schedule_interval="@weekly",
+    catchup=False,
+    tags=['qualifying', 'historical_load']
+) as dag:
+    
+    execution_date = f'{Variable.get("execution_date")}' # Parámetro para la fecha de ejecución
+    # Tasks
+    load_bronze = SparkSubmitOperator(
+        task_id="load_bronze_qualifying",
+        application=f'{Variable.get("spark_scripts_dir")}/ingest_history_qualifying_to_bronze.py',
+        conn_id="spark_default",
+        dag=dag,
+        driver_class_path=Variable.get("driver_class_path"),
+        application_args=[execution_date],
+        py_files= f'{Variable.get("dags_dir")}/utils/helpers.py'
+    )
+
+    load_silver = SparkSubmitOperator(
+        task_id="transform_silver_qualifying",
+        application=f'{Variable.get("spark_scripts_dir")}/ingest_qualifying_to_silver.py',
+        conn_id="spark_default",
+        dag=dag,
+        driver_class_path=Variable.get("driver_class_path"),
+        application_args=[execution_date],
+        py_files= f'{Variable.get("dags_dir")}/utils/helpers.py'
+    )
+
+    load_bronze >> load_silver
