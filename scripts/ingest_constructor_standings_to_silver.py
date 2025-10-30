2b193ade5d21f2902f6c54d2d6fa5bc12466cacf
Lautaro Poletto <lautaropoletto@gmail.com>
2025-10-30 20:20:45 +0000

    Fix: ingest_constructor_standings_to_silver.py

--- scripts/ingest_constructor_standings_to_silver.py
+++ scripts/ingest_constructor_standings_to_silver.py
@@ -2,8 +2,9 @@ from datetime import datetime
 from os import environ as env
 import sys
 from helpers import *
-from pyspark.sql.functions import to_timestamp, lit, concat, col, when,coalesce,trim
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType,FloatType
+from pyspark.sql.functions import count, sum, when, col, desc, rank, asc, row_number, lit, max
+from pyspark.sql.window import Window
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, FloatType, DateType
 
 BRONZE_LAYER_PATH = env["BRONZE_LAYER_PATH"]
 SILVER_LAYER_PATH = env["SILVER_LAYER_PATH"]
@@ -91,6 +92,49 @@ def ingest_constructor_standings_to_silver(spark, execution_date):
 
     print("\n################## Datos guardados en MinIO con éxito. ##################\n")
     print(f"\n################## {output_path} ##################\n")
+
+    # # Testing data
+    # races_schema = StructType(fields=[StructField("raceId", IntegerType(), False),
+    #                                  StructField("year", IntegerType(), True),
+    #                                  StructField("round", IntegerType(), True),
+    #                                  StructField("circuitId", IntegerType(), True),
+    #                                  StructField("name", StringType(), True),
+    #                                  StructField("date", DateType(), True),
+    #                                  StructField("time", StringType(), True),
+    #                                  StructField("url", StringType(), True)                 
+    # ])
+
+    # file_path_races = f"{BRONZE_LAYER_PATH}/{v_file_date}/races"
+
+    # races_df = spark.read.option("header", True) \
+    # .schema(races_schema) \
+    # .format("csv") \
+    # .load(file_path_races) \
+    # .select(
+    #     col("raceId").alias("race_id"),
+    #     col("year").alias("race_year"),
+    #     col("round")
+    # )
+
+    # races_df_agg = races_df.groupBy("race_year").agg(
+    #     max("round").alias("last_round"),
+    #     max("race_id").alias("race_id")
+    # )
+
+    # constructor_standings_final_df.createOrReplaceTempView("constructor_standings")
+    # races_df_agg.createOrReplaceTempView("races")
+
+    # df = spark.sql(f"""
+    # select distinct
+    #     r.race_year,
+    #     cs.*
+    # from constructor_standings cs
+    # inner join races r on cs.race_id = r.race_id
+    # where cs.`position`=1 and cs.constructor_id=6
+    # """)
+
+    # df.show(20, truncate=False)
+    # df.count()
 
 
 if __name__ == "__main__":
